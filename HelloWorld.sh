85029b09472906b5fdddbf6f753769199a54953d
root <root@ip-172-31-30-147.eu-north-1.compute.internal>
2025-09-25 09:53:28 +0000

    Added Hello World Script

---
+++ HelloWorld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+####################################
+# Author: Rohith
+# Date  : 25/09/2025
+#
+#This script outputs the Helloword
+#
+#Version: V1
+
+
+echo "Hello DevOps Engineer. Welcome to DevOps World!"
+
